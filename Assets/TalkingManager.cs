d00a015c6ac7efe14c22f8327798abb498a9af2a
루뎅 <manglilsdsd@gmail.com>
2021-10-18 10:27:59 +0900

    Dialogue시스템(타자기 이펙트) 봉합 성공

--- Assets/TalkingManager.cs
+++ Assets/TalkingManager.cs
@@ -8,6 +8,7 @@ public class TalkingManager : MonoBehaviour
    public Image actorImage;
     public Text actorName;
     public Text messageText;
+
     public RectTransform backgroundBox;
 
     Message[] currentMessages;
@@ -28,14 +29,28 @@ public class TalkingManager : MonoBehaviour
     
     void DisplayMessage() {
         Message messageToDisplay = currentMessages[activeMessage];
-        if other == true 
-        messageText.text = messageToDisplay.message;
-        else messageText2
+       
+        //messageText.text = messageToDisplay.message;
+        StartCoroutine(TypeSentence(messageToDisplay));//
 
         Actor actorToDisplay = currentActors[messageToDisplay.actorId];
         actorName.text = actorToDisplay.name;
         actorImage.sprite = actorToDisplay.sprite;
+    }   
+
+    IEnumerator TypeSentence(Message messageToDisplay)//
+    {
+        messageText.text = "";
+        foreach(char letter in messageToDisplay.message.ToCharArray()) {
+            yield return new WaitForSeconds(0.07f);
+            messageText.text += letter;
+            yield return null;
+        }
     }
+
+
+
+
 
     public void NextMessage() {
         activeMessage++;
