d0e479e67700eccc69084ba7efbb9cc1e012622c
루뎅 <manglilsdsd@gmail.com>
2021-11-03 18:41:14 +0900

    2021.11.03_6:41pm

--- Assets/BookBoriBookImageChanger.cs
+++ Assets/BookBoriBookImageChanger.cs
@@ -7,16 +7,14 @@ public class BookBoriBookImageChanger : MonoBehaviour
 {
     public List<Sprite> BookImageList;
     public GameObject BookImageObject;
+    public int BookCodesNumber;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     public void FirstImage(){
         if (BookImageList.Count >= 1){
             BookImageObject.GetComponent<Image>().sprite = BookImageList[0];
+            BookCodesNumber = 1;
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
         }
@@ -25,6 +23,7 @@ public class BookBoriBookImageChanger : MonoBehaviour
     public void SecondImage(){
          if (BookImageList.Count >= 2){
             BookImageObject.GetComponent<Image>().sprite = BookImageList[1];
+            BookCodesNumber = 2;
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
         }
@@ -32,12 +31,14 @@ public class BookBoriBookImageChanger : MonoBehaviour
     public void ThirdImage(){
          if (BookImageList.Count >= 3){
             BookImageObject.GetComponent<Image>().sprite = BookImageList[2];
+            BookCodesNumber = 3;
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
         }
     }
     public void ForthImage(){
          if (BookImageList.Count >= 4){
+            BookCodesNumber = 4;
             BookImageObject.GetComponent<Image>().sprite = BookImageList[3];
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
@@ -47,12 +48,14 @@ public class BookBoriBookImageChanger : MonoBehaviour
       public void FifthImage(){
          if (BookImageList.Count >= 5){
             BookImageObject.GetComponent<Image>().sprite = BookImageList[4];
+            BookCodesNumber = 5;
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
         }
     }
       public void SixImage(){
          if (BookImageList.Count >= 6){
+             BookCodesNumber = 6;
             BookImageObject.GetComponent<Image>().sprite = BookImageList[5];
         }else{
             Debug.Log("해당 버튼의 경우의 수까지 지정된 책이 없습니다!!");
---
+++ Assets/BookInfoChecker.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookInfoChecker : MonoBehaviour
+{
+    public GameObject BookImage;
+    public GameObject TitleSectionText;
+    public GameObject WriterSectionText;
+    public GameObject PublicherSectionText;
+    public GameObject CateGoriSectionText;
+    public GameObject OKButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BookImage.GetComponent<BookBoriBookImageChanger>().BookCodesNumber > 0){
+            OKButton.GetComponent<Image>().color = new Color32(73, 167, 221, 255);
+        }
+        
+        if (BookImage.GetComponent<BookBoriBookImageChanger>().BookCodesNumber == 0 ){
+            TitleSectionText.GetComponent<Text>().text = "";
+            WriterSectionText.GetComponent<Text>().text = "";
+            PublicherSectionText.GetComponent<Text>().text = "";
+            CateGoriSectionText.GetComponent<Text>().text= "";
+            OKButton.GetComponent<Image>().color = new Color32(118, 118, 118, 255);
+        }
+        if (BookImage.GetComponent<BookBoriBookImageChanger>().BookCodesNumber == 1 ){
+            TitleSectionText.GetComponent<Text>().text = "어쌔신크리드";
+            WriterSectionText.GetComponent<Text>().text = "올리버 보든";
+            PublicherSectionText.GetComponent<Text>().text = "제우 미디어";
+            CateGoriSectionText.GetComponent<Text>().text= "소설";
+        }
+        if (BookImage.GetComponent<BookBoriBookImageChanger>().BookCodesNumber == 2 ){
+            TitleSectionText.GetComponent<Text>().text = "지브리의 천재들";
+            WriterSectionText.GetComponent<Text>().text = "스즈키 도시오";
+            PublicherSectionText.GetComponent<Text>().text = "출판출판사";
+            CateGoriSectionText.GetComponent<Text>().text= "다큐멘터리";
+        }
+ 
+
+    }
+}
