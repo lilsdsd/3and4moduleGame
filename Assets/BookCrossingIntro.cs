c48f2cfb9901dd43985dbd3d6e28fd9ac1262f7f
루뎅 <manglilsdsd@gmail.com>
2021-11-10 14:30:51 +0900

    2021.11.10_2:30pm

--- Assets/BookCrossingIntro.cs
+++ Assets/BookCrossingIntro.cs
@@ -7,8 +7,14 @@ using UnityEngine.UI;
 public class BookCrossingIntro : MonoBehaviour
 {
 
+    public List<GameObject> obj;
+
     void Awake(){
        LeanTween.alpha(gameObject.GetComponent<RectTransform>(), 0f, 3f).setDelay(0f);
+        for(int i = 0; i < obj.Count; i++){
+            LeanTween.alpha(obj[i].GetComponent<RectTransform>(), 0f, 3f).setDelay(0f);
+            obj[i].SetActive(false);
+        }
       // gameObject.setActive(false);
     }
     // Start is called before the first frame update
@@ -17,9 +23,10 @@ public class BookCrossingIntro : MonoBehaviour
         LeanTween.alpha(gameObject.GetComponent<RectTransform>(), 1f, 3f).setDelay(1f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AImg(){
+
+        obj[0].SetActive(true);
+
+
     }
 }
